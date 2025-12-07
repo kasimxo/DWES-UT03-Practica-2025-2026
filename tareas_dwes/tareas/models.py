2df760f5493b3395066423075348a93b6c2cbba1
kasimxo <kasimxo@gmail.com>
2025-12-07 11:32:04 +0100

    Modelo funcionando

--- tareas_dwes/tareas/models.py
+++ tareas_dwes/tareas/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    id = models.UUIDField(primary_key=True)
+    titulo = models.CharField()
+    descripcion = models.TextField()
+    completada = models.BooleanField(default=False)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_recordatorio = models.DateTimeField()
+
+    def __str__(self):
+        return self.titulo
