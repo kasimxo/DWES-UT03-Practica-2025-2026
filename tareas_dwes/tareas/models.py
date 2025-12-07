a453bc9d523acd3f7473df009e146c389f227d82
kasimxo <kasimxo@gmail.com>
2025-12-07 12:45:31 +0100

    Vistas funcionando

--- tareas_dwes/tareas/models.py
+++ tareas_dwes/tareas/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Tarea(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True) # UUID de ejemplo: 42536292-a95b-422f-a411-d877c260022e
     titulo = models.CharField()
     descripcion = models.TextField()
     completada = models.BooleanField(default=False)
---
+++ tareas_dwes/tareas/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .views import detalle_tarea
+
+urlpatterns = [
+    path('<uuid:pk>/', detalle_tarea.as_view(), name='detalle_tarea'),
+]
